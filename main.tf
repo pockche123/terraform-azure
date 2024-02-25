4869019c23aba1e84d62ec2343c8a1645017cc10
pockche123 <130081044+pockche123@users.noreply.github.com>
2024-02-25 17:17:12 +0000

    update resource

--- main.tf
+++ main.tf
@@ -1,13 +1,34 @@
 terraform {
-    required_providers {
-        azurerm ={
-            source = "hashicorp/azurerm"
-            version = "=3.0.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 0.0.0"
     }
-    
+  }
 }
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-    feature{}
+  skip_provider_registration = true
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_virtual_network" "vn" {
+    name = "v-network"
+    resource_group_name = azurerm_resource_group.example.name
+    location = azurerm_resource_group.example.location
+    address_space = ["10.123.0.0/16"]
+
+    tags = {
+        environment = "dev"
+    }
 }
